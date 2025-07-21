820361e8b6ee7eafdd1f65b203604b51a56037f9
Juan Galicia <68486813+Juancho2004gg@users.noreply.github.com>
2025-07-20 23:25:27 -0400

    appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
